46e9b3fb172e048b093ea2d6ccc1697e32968e68
Irving Herrera <irving.herreramolina@gmail.com>
2020-01-13 23:49:36 -0600

    Estructura inicial de la pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BarComponent } from './shared/bar/bar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
