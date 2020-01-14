99373e5e20eea17111f0fa205703dd299a3b3c0d
Irving Herrera <irving.herreramolina@gmail.com>
2020-01-14 00:21:17 -0600

    camponentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BarComponent } from './shared/bar/bar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/home/about/about.component';
+import { ExperienceComponent } from './pages/home/experience/experience.component';
+import { EducationComponent } from './pages/home/education/education.component';
+import { SkillsComponent } from './pages/home/skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule
